2fbb5262f6e8b88bc6b06af7018c316a1ebb7282
ljh3465 <ms5313@naver.com>
2025-06-22 20:43:36 +0900

    애드센스 메타정보 등록

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
+
+
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <meta name="google-site-verification" content="oKc8NhzU3GBPPmzBASGFjbVPH7H4Dc4DEHZbPEq4vy8" />
+        
         {/* 애드센스 소유권 확인용 코드 삽입 */}
         <script
           async
